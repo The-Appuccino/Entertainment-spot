b40f60767d25045040fb75aef4dc2d32e3d5a7ba
Alexis Santiago <Alexissantiago1213@gmail.com>
2025-04-15 21:06:03 -0400

    -initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,6 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
@@ -45,4 +44,5 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    implementation("androidx.drawerlayout:drawerlayout:1.2.0")
 }
